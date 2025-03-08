dd41e08ae2d5b418db16737706e5f585645e9fea
Viktor <79509631+bohatko@users.noreply.github.com>
2025-03-09 01:15:49 +0300

    Delete lib directory

--- lib/backend/schema/structs/index.dart
+++
@@ -1,7 +0,0 @@
-export '/backend/schema/util/schema_util.dart';
-
-export 'filter_home_orders_struct.dart';
-export 'filter_home_project_struct.dart';
-export 'message_struct.dart';
-export 'selected_dop_struct.dart';
-export 'selected_teknik_struct.dart';
--- lib/custom_code/actions/index.dart
+++
@@ -1,3 +0,0 @@
-export 'download_file.dart' show downloadFile;
-export 'generate_and_download_csv.dart' show generateAndDownloadCsv;
-export 'lock_orientation.dart' show lockOrientation;
