341c5ae80ed131326ef7944c17a9f60b1c9d65e3
Viktor <79509631+bohatko@users.noreply.github.com>
2025-03-09 01:16:48 +0300

    Delete firebase directory

--- firebase/custom_cloud_functions/index.js
+++
@@ -1,7 +0,0 @@
-const admin = require("firebase-admin/app");
-admin.initializeApp();
-
-const simpleReplaceInDocx = require("./simple_replace_in_docx.js");
-exports.simpleReplaceInDocx = simpleReplaceInDocx.simpleReplaceInDocx;
-const cancelOrders = require("./cancel_orders.js");
-exports.cancelOrders = cancelOrders.cancelOrders;
